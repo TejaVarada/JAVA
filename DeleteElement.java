1389e8ac01766930dfac6bd3633fea42245fc52d
tejakool311 <tejakool311@gmail.com>
2024-05-21 20:49:15 -0400

    Update DeleteElement File

--- DeleteElement.java
+++ DeleteElement.java
@@ -1,25 +1,36 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+ 
 public class DeleteElement{
     public static void main(String[] args){
-
+ 
         Scanner sc = new Scanner(System.in);
-        int i,j,s,t=0,c=0;
-
+ 
         System.out.print("Enter Array Size: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
-
+ 
         System.out.print("Enter Array Elements: ");
-        for(i=0; i<n; i++) arr[i] = sc.nextInt();
-
-        Array
-        for(i=0; i<n; i++) {
-
-            t=0;
-            for(j=0;j<n;j++)
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
         }
-
-        System.out.println("Hello World");
+ 
+        Arrays.sort(arr);
+ 
+        int previousIdx = 0;
+        int counter = 1;
+ 
+        for(int i=1; i<n; i++) {
+            if(arr[previousIdx] == arr[i]) {
+                counter++;
+            } else {
+                System.out.println(arr[previousIdx] + " - " + counter);
+                counter = 1;
+            }
+            previousIdx = i;
+        }
+ 
+        // Print the last element and its count
+        System.out.println(arr[previousIdx] + " - " + counter);
+        
     }
-}
