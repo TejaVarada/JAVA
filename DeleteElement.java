07ef27ba1072d4cc2859d5e38be4b618dbcb4917
tejakool311 <tejakool311@gmail.com>
2024-05-21 20:28:28 -0400

    Update DeleteElement File

--- DeleteElement.java
+++ DeleteElement.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 
 public class DeleteElement{
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int i,j,s,t=0,c=0;
+
+        System.out.print("Enter Array Size: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.print("Enter Array Elements: ");
+        for(i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        Array
+        for(i=0; i<n; i++) {
+
+            t=0;
+            for(j=0;j<n;j++)
+        }
+
         System.out.println("Hello World");
     }
 }
